ab710cec524fb000f96e2e78bc6685e8ef4d1cd7
curlydev07 <reggie.frias1105@gmail.com>
2023-06-08 23:42:14 +0800

    summary filter

--- resources/views/admin/orders/history.blade.php
+++ resources/views/admin/orders/history.blade.php
@@ -2,6 +2,8 @@
 
 
 @section('page')
+<i class="fa-sharp fa-solid fa-arrow-up"></i>
+<i class="fas fa-sort-circle-up"></i>
     <div class="tbg-white tpb-5 trounded-lg tshadow-lg ttext-black-100">
         <div class="tborder-b tflex titems-center tjustify-between tpx-5 tpy-3">
             <span class="ttext-base ttext-title tfont-medium">Orders List</span>
@@ -14,7 +16,7 @@
                         </button>
                     </form>
                 </li><!-- SEARCH -->
-                <li class="tmr-4 tpt-1">
+                {{-- <li class="tmr-4 tpt-1">
                     @if (request()->sort == 'asc')
                         <a href="{{ request()->fullUrlWithQuery(['sort' => 'desc']) }}" class="tooltipped" data-position="top" data-tooltip="Sort by newest">
                             <i class="material-icons grey-text tmr-3">sort_by_alpha</i>
@@ -24,7 +26,12 @@
                             <i class="material-icons grey-text">sort_by_alpha</i>
                         </a>
                     @endif
-                </li><!-- SORT -->
+                </li><!-- SORT --> --}}
+                <li>
+                    <a onclick="sortHigh()" class="tooltipped ttext-xl tcursor-pointer" data-position="top" data-tooltip="Sort by AVG High to Low">
+                        <svg class="tmr-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M107.31 36.69a16 16 0 0 0-22.62 0l-80 96C-5.35 142.74 1.78 160 16 160h48v304a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V160h48c14.21 0 21.38-17.24 11.31-27.31zM400 416h-16V304a16 16 0 0 0-16-16h-48a16 16 0 0 0-14.29 8.83l-16 32A16 16 0 0 0 304 352h16v64h-16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h96a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM330.17 34.91a79 79 0 0 0-55 54.17c-14.27 51.05 21.19 97.77 68.83 102.53a84.07 84.07 0 0 1-20.85 12.91c-7.57 3.4-10.8 12.47-8.18 20.34l9.9 20c2.87 8.63 12.53 13.49 20.9 9.91 58-24.77 86.25-61.61 86.25-132V112c-.02-51.21-48.4-91.34-101.85-77.09zM352 132a20 20 0 1 1 20-20 20 20 0 0 1-20 20z"/></svg>
+                    </a>
+                </li>
                 <li>
                     <a href="/admin/orders/">
                         {{-- <img src="{{ asset('icons/clear_filter.png') }}" class="tooltipped" data-position="top" data-tooltip="Remove filter"> --}}
@@ -33,7 +40,7 @@
             </ul>
         </div>
         <div class="tpx-3 tpy-4 tflex">
-            <table style="width: 100%;">
+            <table style="width: 100%;" id="myTable">
                 <tr>
                     <th>Products</th>
                     @foreach ($dates as $date)
@@ -43,7 +50,8 @@
                 </tr>
 
                     @foreach ($products as $product)
-                        <tr>
+                        <tr class="product">
+                            <td class="thidden hidden_avg"></td>
                             <td>{{ $product->title }}</td> <!-- Foreach Product -->
 
                             @if (count($dates) > 0)
@@ -55,7 +63,7 @@
                             @endif
 
 
-                            <td class="tfont-semibold ttext-md">
+                            <td class="avg tfont-semibold ttext-md">
                                 @if (count($dates) > 0)
                                     @php
                                         $sum = \App\TransactionPorductSummary::select('qty')->where('product_id', $product->id)->whereBetween('date', [$dates[0]['date'], $dates[(count($dates) - 1)]['date']])->sum('qty');
@@ -102,4 +110,50 @@
         });
     });
 </script>
+
+
+<script>
+    function sortHigh() {
+
+        $('.product').each(function( index ) {
+            let avg = $(this).children().last().html();
+            $(this).children().first().html(avg);
+        });
+
+        var table, rows, switching, i, x, y, shouldSwitch;
+        table = document.getElementById("myTable");
+        switching = true;
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /*Loop through all table rows (except the
+            first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
+                one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("TD")[0];
+                y = rows[i + 1].getElementsByTagName("TD")[0];
+                //check if the two rows should switch place:
+                if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
+                and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
+        }
+    }
+</script>
+
+
 @endsection
