11156a2eae53fa5901637336fb01430f27201baf
curlydev07 <reggie.frias1105@gmail.com>
2023-06-19 21:39:30 +0800

    store metrics mode and migration

---
+++ database/migrations/2023_06_19_133053_create_store_metrics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreMetricsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store_metrics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('store_id')->index();
+            $table->integer('sales');
+            $table->integer('order');
+            $table->double('conversion_rate', 8, 2);
+            $table->integer('visitors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store_metrics');
+    }
+}
