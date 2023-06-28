d3a4305eddb7fff2a62dc5d26a595a10cd2f9563
curlydev07 <reggie.frias1105@gmail.com>
2023-06-28 21:26:47 +0800

    fix dashboard

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -31,85 +31,85 @@
         <div class="col s12">
             <ul class="tabs tabs tflex tjustify-between">
                 {{-- <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'dashboard', 'active') }}" onclick="location.href = '/admin/dashboard'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'dashboard', 'active') }}" onclick="location.href = '/admin/dashboard'">
                         <i class="fas fa-desktop tmr-1 fa-lg"></i>
                         Dashboard
                     </a>
                 </li> --}}
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'orders', 'active') }}" onclick="location.href = '/admin/orders'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'orders', 'active') }}" onclick="location.href = '/admin/orders'">
                         <i class="fas fa-shopping-cart tmr-1 fa-lg"></i>
                         Orders
                     </a>
                 </li>
                 {{-- <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'shopee', 'active') }}" onclick="location.href = '/admin/shopee'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'shopee', 'active') }}" onclick="location.href = '/admin/shopee'">
                         <i class="fas fa-shopping-cart tmr-1 fa-lg"></i>
                         Shopee Orders
                     </a>
                 </li> --}}
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'products', 'active') }}" onclick="location.href = '/admin/products'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'products', 'active') }}" onclick="location.href = '/admin/products'">
                         <i class="fas fa-leaf tmr-1 fa-lg"></i> 
                         Products
                     </a>
                 </li>
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'inventory', 'active') }}" onclick="location.href = '/admin/inventory'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'inventory', 'active') }}" onclick="location.href = '/admin/inventory'">
                         <i class="fas fa-comment tmr-1 fa-lg"></i> 
                         Inventory
                     </a>
                 </li>
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'purchase', 'active') }}" onclick="location.href = '/admin/purchase'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'purchase', 'active') }}" onclick="location.href = '/admin/purchase'">
                        <i class="fas fa-shopping-bag tmr-1 fa-lg"></i>
                         Purchase
                     </a>
                 </li>
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'expenses', 'active') }}" onclick="location.href = '/admin/expenses'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'expenses', 'active') }}" onclick="location.href = '/admin/expenses'">
                        <i class="fas fa-users tmr-1 fa-lg"></i>
                         Expenses
                     </a> 
                 </li>
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'suppliers', 'active') }}" onclick="location.href = '/admin/suppliers'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'suppliers', 'active') }}" onclick="location.href = '/admin/suppliers'">
                        <i class="fas fa-users tmr-1 fa-lg"></i>
                         Suppliers
                     </a>
                 </li>
                 {{-- <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'rts', 'active') }}" onclick="location.href = '/admin/rts'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'rts', 'active') }}" onclick="location.href = '/admin/rts'">
                        <i class="fas fa-users tmr-1 fa-lg"></i>
                         RTS
                     </a>
                 </li> --}}
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'stores', 'active') }}" onclick="location.href = '/admin/stores'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'stores', 'active') }}" onclick="location.href = '/admin/stores'">
                         <i class="fas fa-users tmr-1 fa-lg"></i> 
                         Stores
                     </a>
                 </li>
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'store-metrics', 'active') }}" onclick="location.href = '/admin/store-metrics'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'store-metrics', 'active') }}" onclick="location.href = '/admin/store-metrics'">
                         <i class="fas fa-users tmr-1 fa-lg"></i> 
                         Store Metrics
                     </a>
                 </li>
                 <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'fbads', 'active') }}" onclick="location.href = '/admin/fbads'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'fbads', 'active') }}" onclick="location.href = '/admin/fbads'">
                         <i class="fas fa-users tmr-1 fa-lg"></i> 
                         FB
                     </a>
                 </li>
                 {{-- <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'categories', 'active') }}" onclick="location.href = '/admin/categories'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'categories', 'active') }}" onclick="location.href = '/admin/categories'">
                         <i class="fas fa-comment tmr-1 fa-lg"></i> 
                         Categories
                     </a>
                 </li> --}}
                 {{-- <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'banners', 'active') }}" onclick="location.href = '/admin/banners'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'banners', 'active') }}" onclick="location.href = '/admin/banners'">
                         <i class="fas fa-images tmr-1 fa-lg"></i> 
                         Banners
                     </a>
@@ -117,7 +117,7 @@
                
                 
                 {{-- <li class="tab col">
-                    <a href="#" class="{{ is_matched_return_class(admin_parent_nav(), 'inventory', 'active') }}" onclick="location.href = '/admin/inventory'">
+                    <a href="#" style="padding: 0px" class="{{ is_matched_return_class(admin_parent_nav(), 'inventory', 'active') }}" onclick="location.href = '/admin/inventory'">
                         <i class="fas fa-cogs tmr-1 fa-lg"></i> 
                         Inventory
                     </a>
