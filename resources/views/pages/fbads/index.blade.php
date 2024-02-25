94ebb67ea71ab2a90346511347192e1ae2e8fc4f
curlydev07 <reggie.frias1105@gmail.com>
2024-02-25 20:53:06 +0800

    change VIdeo

--- resources/views/pages/fbads/index.blade.php
+++ resources/views/pages/fbads/index.blade.php
@@ -126,7 +126,7 @@
         <img class="tmb-3" src="{{ asset('images\kasoy_oil\28_days_challenge.png') }}" alt="28_days_challenge">
 
         <video class="tw-full video-testimonial" controls playsinline webkit-playsinline>
-            <source src="{{ asset('images/kasoy_oil/testimonial.mp4') }}" type="video/mp4">
+            <source src="{{ asset('images/kasoy_oil/explainer_video.mp4') }}" type="video/mp4">
             <source src="movie.ogg" type="video/ogg">
         </video>
 
