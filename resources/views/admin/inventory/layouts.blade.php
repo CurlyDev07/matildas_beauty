90ea2a157f158df76afacaaa2a0899242b33bbe5
curlydev07 <reggie.frias1105@gmail.com>
2024-08-13 19:19:37 +0800

    layout

--- resources/views/admin/inventory/layouts.blade.php
+++ resources/views/admin/inventory/layouts.blade.php
@@ -3,39 +3,37 @@
 @section('content')
     <div id="profile" class="col s12">
         <div class="ttext-2xl ttext-title tfont-medium tpb-4">Inventory</div>
-        <div class="tflex">
-            <div class="tw-1/4 tmr-10 trounded-lg">
-                <ul class="collection with-header tshadow-lg tm-0 tsticky ttop-0">
-                    <li onclick="window.location.href = '/admin/inventory'" class="collection-item tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/inventory', 'tborder-primary tborder-l-4') }}">
-                        <div class="ttext-md ttext-black-100 tmy-1 ">
-                            Inventory List
-                            <a class="secondary-content">
-                                <i class="fas fa-th-list ttext-primary"></i>
-                            </a>
-                        </div>
-                    </li>
-                    <li onclick="window.location.href = '/admin/inventory/in-out-list'" class="collection-item tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/inventory/in-out-list', 'tborder-primary tborder-l-4') }}">
-                        <div class="ttext-md ttext-black-100 tmy-1 ">
-                           Stock In/Out List
-                            <a class="secondary-content">
-                                <i class="fas fa-th-list ttext-primary"></i>
-                            </a>
-                        </div>
-                    </li>
-                    <li onclick="window.location.href = '/admin/inventory/stock-in'" class="collection-item tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/inventory/stock-in', 'tborder-primary tborder-l-4') }}">
-                        <div class="ttext-md ttext-black-100 tmy-1 ">
-                            Manual Stock-in
-                            <a class="secondary-content">
-                                <i class="fas fa-th-list ttext-primary"></i>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </div><!-- NAV -->
-            <div class="tw-3/4">
-                @yield('page')
-            </div><!-- CONTENT -->
-        </div>
+        <div class="tw-full tmr-10 trounded-lg">
+            <ul class="collection  tbg-white  tflex">
+                <li onclick="window.location.href = '/admin/inventory'" class="tmy-1 tpx-5 tpy-2 tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/inventory', 'tborder-primary tborder-l-4') }}">
+                    <div class="ttext-md ttext-black-100 tmy-1 ">
+                        <a class="tmr-2">
+                            <i class="fas fa-th-list ttext-primary"></i>
+                        </a>
+                        Inventory List
+                    </div>
+                </li>
+                <li onclick="window.location.href = '/admin/inventory/in-out-list'" class="tmy-1 tpx-5 tpy-2 tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/inventory/in-out-list', 'tborder-primary tborder-l-4') }}">
+                    <div class="ttext-md ttext-black-100 tmy-1 ">
+                        <a class="tmr-2">
+                            <i class="fas fa-th-list ttext-primary"></i>
+                        </a>
+                        Stock In/Out List
+                    </div>
+                </li>
+                <li onclick="window.location.href = '/admin/inventory/stock-in'" class="tmy-1 tpx-5 tpy-2 tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/inventory/stock-in', 'tborder-primary tborder-l-4') }}">
+                    <div class="ttext-md ttext-black-100 tmy-1 ">
+                        <a class="tmr-2">
+                            <i class="fas fa-th-list ttext-primary"></i>
+                        </a>
+                        Manual Stock-in
+                    </div>
+                </li>
+            </ul>
+        </div><!-- NAV -->
+        <div class="tw-full">
+            @yield('page')
+        </div><!-- CONTENT -->
     </div>
 @endsection
 
--- resources/views/admin/orders/layouts.blade.php
+++ resources/views/admin/orders/layouts.blade.php
@@ -3,39 +3,37 @@
 @section('content')
     <div id="profile" class="col s12">
         <div class="ttext-2xl ttext-title tfont-medium tpb-4">Orders</div>
-        <div class="tflex">
-            <div class="tw-1/4 tmr-10 trounded-lg">
-                <ul class="collection with-header tshadow-lg tm-0 tsticky ttop-0">
-                    <li onclick="window.location.href = '/admin/orders'" class="collection-item tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/orders', 'tborder-primary tborder-l-4') }}">
-                        <div class="ttext-md ttext-black-100 tmy-1 ">
-                            Order List
-                            <a class="secondary-content">
-                                <i class="fas fa-th-list ttext-primary"></i>
-                            </a>
-                        </div>
-                    </li>
-                    <li onclick="window.location.href = '/admin/orders/create'" class="collection-item tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/orders/create', 'tborder-primary tborder-l-4') }}">
-                        <div class="ttext-md ttext-black-100 tmy-1 ">
-                            Create Order
-                            <a class="secondary-content">
-                                <i class="fas fa-plus-circle ttext-primary"></i>
-                            </a>
-                        </div>
-                    </li>
-                    <li onclick="window.location.href = '/admin/orders/history'" class="collection-item tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/orders/history', 'tborder-primary tborder-l-4') }}">
-                        <div class="ttext-md ttext-black-100 tmy-1 ">
-                            Order History
-                            <a class="secondary-content">
-                                <i class="fas fa-plus-circle ttext-primary"></i>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </div><!-- NAV -->
-            <div class="tw-3/4">    
-                @yield('page')
-            </div><!-- CONTENT -->
-        </div>
+        <div class="tw-full tmr-10 trounded-lg">
+            <ul class="collection  tbg-white  tflex">
+                <li onclick="window.location.href = '/admin/orders'" class="tmy-1 tpx-5 tpy-2 tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/orders', 'tborder-primary tborder-l-4') }}">
+                    <div class="ttext-md ttext-black-100 tmy-1 ">
+                        <a class="tmr-2">
+                            <i class="fas fa-th-list ttext-primary"></i>
+                        </a>
+                        Order List
+                    </div>
+                </li>
+                <li onclick="window.location.href = '/admin/orders/create'" class="tmy-1 tpx-5 tpy-2 tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/orders/create', 'tborder-primary tborder-l-4') }}">
+                    <div class="ttext-md ttext-black-100 tmy-1 ">
+                        <a class="tmr-2">
+                            <i class="fas fa-plus-circle ttext-primary"></i>
+                        </a>
+                        Create Order
+                    </div>
+                </li>
+                <li onclick="window.location.href = '/admin/orders/history'" class="tmy-1 tpx-5 tpy-2 tcursor-pointer waves-block waves-effect hover:tbg-blue-100 {{ is_matched_return_class(url()->current(), url('/').'/admin/orders/history', 'tborder-primary tborder-l-4') }}">
+                    <div class="ttext-md ttext-black-100 tmy-1 ">
+                        <a class="tmr-2">
+                            <i class="fas fa-plus-circle ttext-primary"></i>
+                        </a>
+                        Order History
+                    </div>
+                </li>
+            </ul>
+        </div><!-- NAV -->
+        <div class="tw-full">    
+            @yield('page')
+        </div><!-- CONTENT -->
     </div>
 @endsection
 
