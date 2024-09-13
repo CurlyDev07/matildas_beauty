0c58874fa2d22a3c1005618d266be0f075a1608e
curlydev07 <reggie.frias1105@gmail.com>
2024-09-13 18:59:33 +0800

    bring back the pixel

--- resources/views/pages/fbads/category/home_improvements/layouts/head.blade.php
+++ resources/views/pages/fbads/category/home_improvements/layouts/head.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MissTisa | Matilda's Beauty</title>
+    <title>Smart Light PH | Auto On/Off Bulb Holder</title>
     {{-- STYLE SHEETS --}}
     <link rel="stylesheet" href="{{ asset('css/materialize.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
