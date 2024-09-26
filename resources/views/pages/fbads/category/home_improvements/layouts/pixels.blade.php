0f03607ea3c7e479cb4f3cbabac544d6a62d4eb7
curlydev07 <reggie.frias1105@gmail.com>
2024-09-26 14:14:04 +0800

    add pixel

--- resources/views/pages/fbads/category/home_improvements/layouts/pixels.blade.php
+++ resources/views/pages/fbads/category/home_improvements/layouts/pixels.blade.php
@@ -1,7 +1,7 @@
 
 
     <!-- Meta Pixel Code -->
-    {{-- <script>
+    <script>
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -17,7 +17,7 @@
 
     <noscript><img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=3862987257311979&ev=PageView&noscript=1"/></noscript>
     <!-- End Meta Pixel Code -->
- --}}
+
 
 
 
