d20e30aaa750316229974ff3db07142735b25ef2
curlydev07 <reggie.frias1105@gmail.com>
2024-10-12 21:02:42 +0800

    addred new pixel

--- resources/views/pages/fbads/category/home_improvements/ginger_oil/pixels.blade.php
+++ resources/views/pages/fbads/category/home_improvements/ginger_oil/pixels.blade.php
@@ -1,5 +1,4 @@
-
-    <!-- Meta Pixel Code -->
+<!-- Meta Pixel Code -->
 <script>
     !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -9,15 +8,11 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '715938787417392');
+    fbq('init', '515458114619983');
     fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=715938787417392&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=515458114619983&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Meta Pixel Code -->
-
-
-
-
     
