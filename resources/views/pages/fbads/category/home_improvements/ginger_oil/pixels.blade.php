9f42fc06dd1f0574303602285fa018d2b8ca6b4f
curlydev07 <reggie.frias1105@gmail.com>
2024-10-17 18:16:39 +0800

    new pixel

--- resources/views/pages/fbads/category/home_improvements/ginger_oil/pixels.blade.php
+++ resources/views/pages/fbads/category/home_improvements/ginger_oil/pixels.blade.php
@@ -1,4 +1,5 @@
-<!-- Meta Pixel Code -->
+
+    <!-- Meta Pixel Code -->
 <script>
     !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -8,11 +9,10 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '515458114619983');
+    fbq('init', '450988314137086');
     fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=515458114619983&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=450988314137086&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Meta Pixel Code -->
-    
