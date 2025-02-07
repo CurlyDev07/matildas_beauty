0f597eb281e057194216ff241ab45343baf81ac6
CurlyDev07 <reggie.frias1105@gmail.com>
2025-02-08 01:25:31 +0800

    product picker on product create page

--- resources/views/admin/fbads/create.blade.php
+++ resources/views/admin/fbads/create.blade.php
@@ -37,14 +37,32 @@
                 <div class="text-sm tfont-medium tpx-5 tpy-4 t ttext-title">
                     Order Details
                 </div>
+
+                <div class="tflex tflex-wrap tpx-5">
+                    <div class="tw-1/5 tpx-1">
+
+                        <select id="promo" class="browser-default tfont-normal ttext-sm tmb-2 ttext-black-100">
+                            <option value="" style="color: #f43958">choose ....</option>
+                            <option value="MissTisa|MissTisa_1pc|499" style="color: #f43958">MissTisa 1pc</option>
+                            <option value="MissTisa|MissTisa_2pcs|849" style="color: #f43958">MissTisa 2pcs</option>
+                            <option value="MissTisa Serum|MissTisaSerum_1pc|749" style="color: #ff6f00">Serum 1pc</option>
+                            <option value="MissTisa Serum|MissTisaSerum_2pcs|999" style="color: #ff6f00">Serum 2pcs</option>
+                            <option value="Smart Light Holder|SmartLight_1pc|549" style="color: #12b101">Smart Light 1pc</option>
+                            <option value="Smart Light Holder|SmartLight_2pcs|899" style="color: #12b101">Smart Light 2pcs</option>
+                            <option value="Smart Light Holder|SmartLight_3pcs|1199" style="color: #12b101">Smart Light 3pcs</option>
+                        </select>
+                     
+                    </div>
+                </div>
+
                 <div class="tflex tflex-wrap tpx-5">
                     <div class="tw-1/5 tpx-1">
                         <label for="product" class="tfont-normal ttext-sm tmb-2 ttext-black-100 active">Product</label>
-                        <input type="text" id="product" name="product" class="browser-default form-control" value="" style="padding: 6px;">
+                        <input type="text" disabled id="product" name="product" class="browser-default form-control tbg-gray-200" value="" style="padding: 6px; background-color: #dadada;">
                     </div>
                     <div class="tw-1/5 tpx-1">
                         <label for="promo" class="tfont-normal ttext-sm tmb-2 ttext-black-100 active">Promo</label>
-                        <input type="text" id="promo" name="promo" class="browser-default form-control" value="" style="padding: 6px;">
+                        <input type="text" id="promo" name="promo" class="promo browser-default form-control" value="" style="padding: 6px;">
                     </div>
                     <div class="tw-1/5 tpx-1 tpx-1 tmb-2 lg:tmb-0">
                         <label for="total" class="tfont-normal ttext-sm tmb-2 ttext-black-100">Amount</label>
@@ -58,4 +76,25 @@
             </div>
         </form>
     </div>
+
+
+
+@endsection
+
+
+
+@section('js')
+    <script>
+        $('#promo').change(function () {
+            let val = $(this).val().split("|");;
+
+            let product = val[0];
+            let promo = val[1];
+            let total = val[2];
+
+            $('#product').val(product);
+            $('.promo').val(promo);
+            $('#total').val('1111');
+        });
+    </script>
 @endsection
